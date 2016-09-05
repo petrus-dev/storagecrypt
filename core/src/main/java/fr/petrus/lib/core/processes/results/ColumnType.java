cd30e2253ceb0cdc25b0b119ffe74d763eb5fd53
pierre <pierre.sagne@yahoo.fr>
2016-09-05 19:15:50 +0200

    New enum for results columns types

---
+++ core/src/main/java/fr/petrus/lib/core/processes/results/ColumnType.java
@@ -0,0 +1,14 @@
+package fr.petrus.lib.core.processes.results;
+
+/**
+ * The type of results column
+ *
+ * @author Pierre Sagne
+ * @since 04.09.2016
+ */
+public enum ColumnType {
+    Document,
+    Source,
+    Destination,
+    Error
+}
